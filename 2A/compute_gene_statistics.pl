2228e586695e6f321c8826a4dde4fa46bc5aec87
Ian Goetting <ian@Ians-iMac.jetpack>
2017-10-30 22:36:33 -0600

    modify top50 and compute

--- 2A/compute_gene_statistics.pl
+++ 2A/compute_gene_statistics.pl
@@ -1,33 +1,40 @@
 ###################
 #compute_gene_statistics.pl
 ###################
-
+#
+#Perl script that computes gene statistics for ALL_AML_gr.thr.train.csv. 
+#These statistics include average expression for ALL & AML, standard deviation for ALL & AML, 
+#signal to noise ratio for ALL & AML, T-value for ALL & AML.
+#
+#This script requires the ALL_AML_gr.thr.train.csv file generated from step 1.F of the homework.
+#
 use strict;
 use warnings;
-
+#Function to compute the average expression
 sub average{
     my($Sum_val,$N) = @_;
     return $Sum_val/$N;
 }
-
+#Function to compute the standard deviation
 sub standard_deviation{
     my($N,$Sum_val,$Sum_sq) = @_;
     return sqrt(($N*$Sum_sq - $Sum_val*$Sum_val)/($N*($N-1)));
 }
-
+#Function to compute the signal to noise ratio
 sub signal_to_noise{
     my($Avg1,$Avg2,$Stdev1,$Stdev2) = @_;
     return ($Avg1 - $Avg2)/($Stdev1 + $Stdev2);
 }
-
+#Function to compute the T-value
 sub T_value{
     my($Avg1,$Avg2,$Stdev1,$Stdev2,$N1,$N2) = @_;
     return ($Avg1 - $Avg2) / sqrt($Stdev1 * $Stdev1/$N1 + $Stdev2 * $Stdev2/$N2);
 }
-
+#Function that prints a line to the file depending on the mode and what params are given
 sub print_result{
     my($gene_name,$Avg1,$Avg2,$Stdev1,$Stdev2,$S2N_ALL,$T_value_ALL,$S2N_AML,$T_value_AML,$mode) = @_;
     my $result_string = "";
+    #Print to the output file depending on what mode is used.
     if($mode eq "S2N_ALL"){$result_string = $gene_name.",".$S2N_ALL."\n";}
     elsif($mode eq "S2N_AML"){$result_string = $gene_name.",".$S2N_AML."\n";}
     elsif($mode eq "T_value_ALL"){$result_string = $gene_name.",".$T_value_ALL."\n";}
@@ -35,7 +42,16 @@ sub print_result{
     else{$result_string = $gene_name.",".$Avg1.",".$Avg2.",".$Stdev1.",".$Stdev2.",".$S2N_ALL.",".$T_value_ALL.",".$S2N_AML.",".$T_value_AML."\n";}
     return $result_string;
 }
-
+#
+#Function that calculates the gene stats and outputs to a file depending on the mode given.
+#mode can be: S2N_ALL, S2N_AML, T_value_AML, T_value_ALL, or default.
+#
+#S2N_ALL prints only the S2N ratio for ALL along with the gene name.
+#S2N_AML prints only the S2N ratio for AML along with the gene name.
+#T_value_ALL prints only the T-value for ALL along with the gene name.
+#T_value_AML prints only the T-value for AML along with the gene name.
+#default prints all statistics incliding averages, standard deviations, s2n ratios, and t-values.
+#
 sub calculate_stats{
     my($input_filename, $output_filename, $mode) = @_;    
     open(my $read_file, "<", $input_filename);
@@ -44,6 +60,7 @@ sub calculate_stats{
     while(my $row = <$read_file>){
         if($skip_ID_row){
             $skip_ID_row = 0;
+            #Print header depending on the mode.
             if($mode eq "S2N_ALL"){print $write_file "Gene_Name,S2N_ALL\n";}
             elsif($mode eq "S2N_AML"){print $write_file "Gene_Name,S2N_AML\n";}
             elsif($mode eq "T_value_ALL"){print $write_file "Gene_Name,T_value_ALL\n";}
@@ -58,6 +75,7 @@ sub calculate_stats{
                    $Sum_val_1 += $row_array[$i];
                    $Sum_sq_1 += ($row_array[$i] * $row_array[$i]);
             }
+            #Calculate avg and stdev for ALL
             my $Avg1 = average($Sum_val_1,$N1);
             my $Stdev1 = standard_deviation($N1,$Sum_val_1,$Sum_sq_1);
             my($N2, $Sum_val_2, $Sum_sq_2) = (38,0,0);
@@ -65,10 +83,13 @@ sub calculate_stats{
                 $Sum_val_2 += $row_array[$i];
                 $Sum_sq_2 += ($row_array[$i] * $row_array[$i]);
             }
+            #Calculate avg and stdev for AML
             my $Avg2 = average($Sum_val_2,$N2);
             my $Stdev2 = standard_deviation($N2,$Sum_val_2,$Sum_sq_2);
+            #Calculate S2N and T-Value for ALL
             my $S2N_ALL = signal_to_noise($Avg1,$Avg2,$Stdev1,$Stdev2);
             my $T_value_ALL = T_value($Avg1,$Avg2,$Stdev1,$Stdev2,$N1,$N2);
+            #Calculate S2N and T-Value for AML
             my $S2N_AML = signal_to_noise($Avg2,$Avg1,$Stdev2,$Stdev1);
             my $T_value_AML = T_value($Avg2,$Avg1,$Stdev2,$Stdev1,$N2,$N1);
             my $result_string = print_result($gene_name,$Avg1,$Avg2,$Stdev1,$Stdev2,$S2N_ALL,$T_value_ALL,$S2N_AML,$T_value_AML,$mode); 
--- 2D/top50.pl
+++ 2D/top50.pl
@@ -2,7 +2,14 @@
 #top50.pl
 #############
 #
+#Perl script that counts how many genes are: 
+#1. shared between S2N_ALL_out.txt and T_value_ALL_out.txt
+#2. shared between S2N_AML_out.txt and T_value_AML_out.txt
+#The results of the counts are then printed to the terminal.
 #
+#This script requires S2N_ALL_out.txt, S2N_AML_out.txt, T_value_ALL_out.txt, and T_value_AML_out.txt
+#These files are generated from step 2A of this homework. 
+#It is assumed these files are already sorted in descending order. 
 #
 use strict;
 use warnings;
@@ -13,7 +20,9 @@ sub top50{
     open(my $S2N_AML_read, "<", $S2N_AML);
     open(my $T_value_AML_read, "<", $T_value_AML);
     my($ALL_count, $AML_count) = (0,0);
-    for(my $i = 0; $i <= 51; $i++){
+    #for each gene in the S2N files
+    for(my $i = 0; $i <= 50; $i++){
+        #Skip the header row.
         if($i == 0){
             <$S2N_ALL_read>;
             <$T_value_ALL_read>;
@@ -21,21 +30,30 @@ sub top50{
             <$T_value_AML_read>;
         }
         else {
+            #Read a gene from S2N files
             my @S2N_ALL_arr = (split(/,/,<$S2N_ALL_read>));
             my @S2N_AML_arr = (split(/,/,<$S2N_AML_read>));
-            for(my $j = 1; $j <= 51; $j++){
+            #for each gene in the T-value files
+            for(my $j = 1; $j <= 50; $j++){
+                #Read a gene from T-value files
                 my @T_value_AML_arr = (split(/,/,<$T_value_AML_read>));
                 my @T_value_ALL_arr = (split(/,/,<$T_value_ALL_read>));
-                print $S2N_ALL_arr[0] . " , " . $T_value_ALL_arr[0]. "\n";
+                #If there is a match, increment the count
                 if($S2N_ALL_arr[0] eq $T_value_ALL_arr[0]){$ALL_count++;}
                 if($S2N_AML_arr[0] eq $T_value_AML_arr[0]){$AML_count++;}
             }
+            #reset the T values back to the start of the file
             seek($T_value_ALL_read,0,0);
             seek($T_value_AML_read,0,0);
+            #skip over the header row again
             <$T_value_ALL_read>;
             <$T_value_AML_read>;
         }
     }
+    close($S2N_ALL_read);
+    close($T_value_ALL_read);
+    close($S2N_AML_read);
+    close($T_value_AML_read);
     print "The number of top 50 common samples for ALL is: ".$ALL_count."\n";
     print "The number of top 50 common samples for AML is: ".$AML_count."\n";
 }
